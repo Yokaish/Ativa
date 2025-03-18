0a3546808959d598ee24b185d3645f9af6033d95
SudreErick <sudrerick@gmail.com>
2025-03-18 12:42:27 -0300

    Adicionando model das coleções funcionario e feedbacks

---
+++ app/src/main/java/br/com/ativa/model/funcionarios.kt
@@ -0,0 +1,12 @@
+package br.com.ativa.model
+
+import com.google.gson.annotations.SerializedName
+
+
+data class funcionarios(
+    @SerializedName("id")val id: String,
+    @SerializedName("nome")val nome: String,
+    @SerializedName("cargo")val cargo: String,
+    @SerializedName("area")val area: String,
+    @SerializedName("data_inicio")val dataInicio: String
+)
